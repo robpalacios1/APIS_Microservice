255e61edbc46ca9fe8e9afd5f57d3fadeda7d102
roberto <robpalacios1@hotmail.com>
2025-08-26 10:29:53 -0500

    Create Class into dataTransferObject

---
+++ src/main/java/com/gestion_proyectos/api/dto/AsignarLiderDTO.java
@@ -0,0 +1,14 @@
+package com.gestion_proyectos.api.dto;
+
+public class AsignarLiderDTO {
+
+  private String liderId;
+
+  public String getLiderId(){
+    return liderId;
+  }
+
+  public void setLiderId(String liderId){
+    this.liderId = liderId;
+  }
+}
