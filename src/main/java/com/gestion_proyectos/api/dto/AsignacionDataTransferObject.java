492b13524286f721e67749ace6f73b17a22caf2d
roberto <robpalacios1@hotmail.com>
2025-08-27 12:04:07 -0500

    Create a Class in DTO

---
+++ src/main/java/com/gestion_proyectos/api/dto/AsignacionDataTransferObject.java
@@ -0,0 +1,15 @@
+package com.gestion_proyectos.api.dto;
+
+public class AsignacionDataTransferObject {
+
+  private Long usuarioId;
+
+  // Getter and Setters transport ID of the user to be assigned
+  public Long getUsuarioId() {
+    return usuarioId;
+  }
+
+  public void setUsuarioId(Long usuarioId) {
+    this.usuarioId = usuarioId;
+  }
+}
