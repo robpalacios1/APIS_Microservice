b88e49c85c7f8c4de861292a9149ccaa5a3ee8df
roberto <robpalacios1@hotmail.com>
2025-08-27 09:48:03 -0500

    Create Usuario Class Entity

---
+++ src/main/java/com/gestion_proyectos/api/entity/Usuario.java
@@ -0,0 +1,41 @@
+package com.gestion_proyectos.api.entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class Usuario {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nombre;
+  private String email;
+
+  // Getters and Setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
