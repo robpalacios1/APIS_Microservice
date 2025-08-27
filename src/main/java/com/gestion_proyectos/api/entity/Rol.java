77c680282f1118d07b2fe4749048af5c62a57848
roberto <robpalacios1@hotmail.com>
2025-08-27 09:46:22 -0500

    add @Entity in public Class and add import file

--- src/main/java/com/gestion_proyectos/api/entity/Rol.java
+++ src/main/java/com/gestion_proyectos/api/entity/Rol.java
@@ -1,9 +1,11 @@
 package com.gestion_proyectos.api.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Rol {
 
   @Id
