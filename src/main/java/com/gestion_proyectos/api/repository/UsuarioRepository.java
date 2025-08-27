01ad425c3a15788fa005dadcd3b9c0d65ff486e2
roberto <robpalacios1@hotmail.com>
2025-08-27 11:28:58 -0500

    Create UsuarioRepositorio.java interface

---
+++ src/main/java/com/gestion_proyectos/api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.gestion_proyectos.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.gestion_proyectos.api.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
