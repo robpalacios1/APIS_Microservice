4e3282657af1b8387e951235d6f0dae974aba3d3
roberto <robpalacios1@hotmail.com>
2025-08-28 16:31:19 -0500

    Create a Class for obtain data to JSON

---
+++ src/main/java/com/gestion_proyectos/api/dto/CrearUsuarioDataTransferObject.java
@@ -0,0 +1,34 @@
+package com.gestion_proyectos.api.dto;
+
+public class CrearUsuarioDataTransferObject {
+
+  private String nombre;
+  private String email;
+  //Here we receive the "Rol" or "Developers"
+  private String rol;
+
+  // Getters and Setters
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getRol() {
+    return rol;
+  }
+
+  public void setRol(String password) {
+    this.rol = password;
+  }
+}
