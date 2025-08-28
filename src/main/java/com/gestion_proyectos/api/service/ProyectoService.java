1629b09491e81951aa2aa2917ef7cf131d231be1
roberto <robpalacios1@hotmail.com>
2025-08-28 09:39:55 -0500

    Refactor in ProyectService.java file

--- src/main/java/com/gestion_proyectos/api/service/ProyectoService.java
+++ src/main/java/com/gestion_proyectos/api/service/ProyectoService.java
@@ -2,7 +2,9 @@ package com.gestion_proyectos.api.service;
 
 import com.gestion_proyectos.api.dto.CrearProyectoDataTransferObject;
 import com.gestion_proyectos.api.entity.Proyecto;
+import com.gestion_proyectos.api.entity.Usuario;
 import com.gestion_proyectos.api.repository.ProyectoRepository;
+import com.gestion_proyectos.api.repository.UsuarioRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,30 +19,39 @@ public class ProyectoService {
   @Autowired
   private ProyectoRepository proyectoRepository;
 
-  public Proyecto crearProyecto(CrearProyectoDataTransferObject crearProyectoDataTransferObject) {
-    //
-    Proyecto nuevoProyecto = new Proyecto();
+  @Autowired
+  private UsuarioRepository usuarioRepository;
 
-    //
+  public Proyecto crearProyecto(CrearProyectoDataTransferObject crearProyectoDataTransferObject) {
+
+    Proyecto nuevoProyecto = new Proyecto();
     nuevoProyecto.setNombre(crearProyectoDataTransferObject.getNombre());
     nuevoProyecto.setDescripcion(crearProyectoDataTransferObject.getDescripcion());
     nuevoProyecto.setFechaInicio(crearProyectoDataTransferObject.getFechaInicio());
 
-    //
     return proyectoRepository.save(nuevoProyecto);
   }
 
   public List<Proyecto> obtenerProyectos() {
     return proyectoRepository.findAll();
   }
-  public Optional<Proyecto> asignarLider(Long proyectoId, String liderId) {
+  public Optional<Proyecto> asignarLider(Long proyectoId, Long liderId) {
+    // Search project
     Optional<Proyecto> proyectoOptional = proyectoRepository.findById(proyectoId);
-    // if project exists
-    if (proyectoOptional.isPresent()) {
+
+    //Search user, it will be assigned as new leader
+    Optional<Usuario> usuarioOptional = usuarioRepository.findById(liderId);
+
+    // if both exists, it connect it
+    if (proyectoOptional.isPresent() && usuarioOptional.isPresent()) {
       Proyecto proyecto = proyectoOptional.get();
-      proyecto.setLider(liderId); // Asign lider to project
-      proyectoRepository.save(proyecto); // Save changes in Data Base
+      Usuario nuevoLider = usuarioOptional.get();
+
+      proyecto.setLider(nuevoLider); // Assign the user
+      proyectoRepository.save(proyecto); // Save changes
+      return Optional.of(proyecto);
     }
-    return proyectoOptional;
+    // if not exists, return empty
+    return Optional.empty(); // If not exists, return empty
   }
 }
