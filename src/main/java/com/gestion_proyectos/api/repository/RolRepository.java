b684dde7d808fd6da9c524d80e2bc3018f9340de
roberto <robpalacios1@hotmail.com>
2025-08-29 10:38:00 -0500

    Search a rol in DB for name

--- src/main/java/com/gestion_proyectos/api/repository/RolRepository.java
+++ src/main/java/com/gestion_proyectos/api/repository/RolRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.gestion_proyectos.api.entity.Rol;
 
+import java.util.Optional;
+
 @Repository
 public interface RolRepository extends JpaRepository<Rol, Long> {
-
+  Optional<Rol> findByNombre(String nombre);
 }
