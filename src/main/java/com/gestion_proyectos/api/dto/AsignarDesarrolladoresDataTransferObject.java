0768373057d67423855e50114dab9909895836a9
roberto <robpalacios1@hotmail.com>
2025-09-01 10:34:38 -0500

    Create Class in DTO to assign developer

---
+++ src/main/java/com/gestion_proyectos/api/dto/AsignarDesarrolladoresDataTransferObject.java
@@ -0,0 +1,16 @@
+package com.gestion_proyectos.api.dto;
+
+import java.util.List;
+
+public class AsignarDesarrolladoresDataTransferObject {
+
+  private List<Long> desarrolladoresDataTransferObject;
+
+  // Getters and Setters
+  public List<Long> getDesarrolladoresDataTransferObject() {
+    return desarrolladoresDataTransferObject;
+  }
+  public void setDesarrolladoresDataTransferObject(List<Long> desarrolladoresDataTransferObject) {
+    this.desarrolladoresDataTransferObject = desarrolladoresDataTransferObject;
+  }
+}
