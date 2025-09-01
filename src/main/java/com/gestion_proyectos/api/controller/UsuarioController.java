97d235999e4bc5b5673aa714840cdddd5791c040
roberto <robpalacios1@hotmail.com>
2025-09-01 09:24:50 -0500

    Create Class in Controller for User

---
+++ src/main/java/com/gestion_proyectos/api/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.gestion_proyectos.api.controller;
+
+
+import com.gestion_proyectos.api.dto.CrearUsuarioDataTransferObject;
+import com.gestion_proyectos.api.entity.Usuario;
+import com.gestion_proyectos.api.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+  @Autowired
+  private UsuarioService usuarioService;
+
+  @PostMapping
+  public ResponseEntity<Usuario> registrarUsuario(@RequestBody CrearUsuarioDataTransferObject usuarioDataTransferObject) {
+    Usuario nuevoUsuario = usuarioService.crearUsuario(usuarioDataTransferObject);
+    return new ResponseEntity<>(nuevoUsuario, HttpStatus.CREATED);
+  }
+}
