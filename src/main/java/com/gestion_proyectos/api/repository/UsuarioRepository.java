6457c0da14887070b65b61b18554f0d5aaa68524
roberto <robpalacios1@hotmail.com>
2025-09-01 14:31:22 -0500

    Add method to search users by email

--- src/main/java/com/gestion_proyectos/api/repository/UsuarioRepository.java
+++ src/main/java/com/gestion_proyectos/api/repository/UsuarioRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.gestion_proyectos.api.entity.Usuario;
 
+import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+  Optional<Usuario> findByEmail(String email);
 }
