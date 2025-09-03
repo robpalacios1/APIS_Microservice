c94a27ce85cc4dda288c884719b1f21614b19b1e
roberto <robpalacios1@hotmail.com>
2025-09-03 16:56:37 -0500

    Add Schemas

--- src/main/java/com/gestion_proyectos/api/dto/LoginRequest.java
+++ src/main/java/com/gestion_proyectos/api/dto/LoginRequest.java
@@ -1,8 +1,14 @@
 package com.gestion_proyectos.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Datos de inicio de sesión")
 public class LoginRequest {
 
+  @Schema(description = "Email del usuario", example = "admin@example.com", required = true)
   private String email;
+
+  @Schema(description = "Contraseña del usuario", example = "admin123", required = true)
   private String password;
 
   // Constructors
