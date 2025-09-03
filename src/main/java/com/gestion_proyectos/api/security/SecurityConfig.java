edee4b2ecb54720bd092421af47870d8306893d3
roberto <robpalacios1@hotmail.com>
2025-09-03 09:42:03 -0500

    add import and Password encoder

--- src/main/java/com/gestion_proyectos/api/security/SecurityConfig.java
+++ src/main/java/com/gestion_proyectos/api/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +21,11 @@ public class SecurityConfig {
 
   @Autowired
   private JwtRequestFilter jwtRequestFilter;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
